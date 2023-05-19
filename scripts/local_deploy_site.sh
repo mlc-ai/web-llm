4acfb1e289c189b6d194c8b24e2ece622c138b52
Ruihang Lai <ruihangl@cs.cmu.edu>
2023-05-19 16:23:24 -0700

    Update local deployment scripts and related fix (#102)
    This PR separates local deployment config from global deployment config,
    fixes related deployment scripts, and updates the publicly available
    wasm/weights links.

--- scripts/local_deploy_site.sh
+++ scripts/local_deploy_site.sh
@@ -7,20 +7,27 @@ if [ -z ${MLC_LLM_HOME_SET} ]; then
     export MLC_LLM_HOME="${MLC_LLM_HOME:-mlc-llm}"
 fi
 
-scripts/build_site.sh web/global_config.json
+scripts/build_site.sh web/local-config.json
 
 echo "symlink parameter location to site.."
 
 if [ -d "$MLC_LLM_HOME/dist/vicuna-v1-7b-q4f32_0/params" ]; then
-    rm -rf site/_site/dist/vicuna-v1-7b-q4f32_0-params
+    rm -rf site/_site/dist/vicuna-v1-7b-q4f32_0
+    mkdir -p site/_site/dist/vicuna-v1-7b-q4f32_0
     ln -s $MLC_LLM_HOME/dist/vicuna-v1-7b-q4f32_0/params site/_site/dist/vicuna-v1-7b-q4f32_0/params
-    ls site/_site/dist/vicuna-v1-7b-q4f32_0
+    cp -rf $MLC_LLM_HOME/dist/vicuna-v1-7b-q4f32_0/vicuna-v1-7b-q4f32_0-webgpu.wasm site/_site/dist/vicuna-v1-7b-q4f32_0/
+fi
+if [ -d "$MLC_LLM_HOME/dist/RedPajama-INCITE-Chat-3B-v1-q4f32_0/params" ]; then
+    rm -rf site/_site/dist/RedPajama-INCITE-Chat-3B-v1-q4f32_0
+    mkdir -p site/_site/dist/RedPajama-INCITE-Chat-3B-v1-q4f32_0
+    ln -s $MLC_LLM_HOME/dist/RedPajama-INCITE-Chat-3B-v1-q4f32_0/params site/_site/dist/RedPajama-INCITE-Chat-3B-v1-q4f32_0/params
+    cp -rf $MLC_LLM_HOME/dist/RedPajama-INCITE-Chat-3B-v1-q4f32_0/RedPajama-INCITE-Chat-3B-v1-q4f32_0-webgpu.wasm site/_site/dist/RedPajama-INCITE-Chat-3B-v1-q4f32_0/
 fi
 if [ -d "$MLC_LLM_HOME/dist/wizardlm-7b/params" ]; then
-    rm -rf site/_site/dist/wizardlm-7b-params
-    ln -s $MLC_LLM_HOME/dist/wizardlm-7b/params site/_site/dist/wizardlm-7b-params
+    rm -rf site/_site/dist/wizardlm-7b
+    mkdir -p site/_site/dist/wizardlm-7b
+    ln -s $MLC_LLM_HOME/dist/wizardlm-7b/params site/_site/dist/wizardlm-7b/params
+    cp -rf $MLC_LLM_HOME/dist/wizardlm-7b/wizardlm-7b-webgpu.wasm site/_site/dist/wizardlm-7b/
 fi
-
-
 
 cd site && jekyll serve  --skip-initial-build --host localhost --baseurl /web-llm --port 8888
