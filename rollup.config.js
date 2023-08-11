48bd00b1ec03ed2ed7083dcd4f309f9cdbed6fc3
Lantianyou <dtlantianyou@gmail.com>
2023-08-11 12:39:21 +0800

    feat: change package method

--- rollup.config.js
+++ rollup.config.js
@@ -1,29 +1,67 @@
 import { nodeResolve } from '@rollup/plugin-node-resolve';
-import ignore from "rollup-plugin-ignore";
-import commonjs from '@rollup/plugin-commonjs';
 import typescript from 'rollup-plugin-typescript2'
+import { defineConfig } from 'rollup';
+import babel from '@rollup/plugin-babel'
+import replace from '@rollup/plugin-replace'
+import terser from '@rollup/plugin-terser'
 
-export default {
+const extensions = ['.ts']
+const noDeclarationFiles = { compilerOptions: { declaration: false } }
+
+const external = []
+
+export default defineConfig([
+    // es
+    {
+        input: 'src/index.ts',
+        output: [
+            {
+                file: 'dist/es/index.js',
+                format: 'es',
+                indent: false,
+            }],
+        external,
+        plugins: [
+            nodeResolve({
+                extensions
+            }),
+            typescript({
+                tsconfigOverride: noDeclarationFiles
+            }),
+            babel({
+                extensions,
+                babelHelpers: 'bundled',
+
+            })
+        ]
+    },
+    // ES for Browsers
+  {
     input: 'src/index.ts',
-    output: [
-        {
-            file: 'lib/index.js',
-            exports: 'named',
-            format: 'es',
-            sourcemap: true,
-            globals: {'ws': 'ws',
-                      'perf_hooks': 'perf_hooks'}
-        }
-    ],
+    output: { file: 'dist/es/redux.mjs', format: 'es', indent: false },
     plugins: [
-        ignore(["fs", "path", "crypto"]),
-        nodeResolve({ browser: true }),
-        commonjs({
-            ignoreDynamicRequires: true,
-        }),
-        typescript({
-            rollupCommonJSResolveHack: false,
-            clean: true
-        })
+      nodeResolve({
+        extensions
+      }),
+      replace({
+        preventAssignment: true,
+        'process.env.NODE_ENV': JSON.stringify('production')
+      }),
+      typescript({ tsconfigOverride: noDeclarationFiles }),
+      babel({
+        extensions,
+        exclude: 'node_modules/**',
+        plugins: [['./scripts/mangleErrors.cjs', { minify: true }]],
+        skipPreflightCheck: true,
+        babelHelpers: 'bundled'
+      }),
+      terser({
+        compress: {
+          pure_getters: true,
+          unsafe: true,
+          unsafe_comps: true
+        }
+      })
     ]
-};
+  }
+])
