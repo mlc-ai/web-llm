c16b38e02db205f7870a2005fdff60f8d88ace21
Sudeep Agarwal <hello@sudeep.co>
2023-08-25 20:47:43 -0400

    Checkpoint

--- examples/chrome-extension/src/background.ts
+++ examples/chrome-extension/src/background.ts
@@ -1,4 +1,4 @@
-import {ChatRestModule} from "@mlc-ai/web-llm";
+import {ChatModule, ChatRestModule, AppConfig} from "@mlc-ai/web-llm";
 
 
 // Set reponse callback for chat module
@@ -7,12 +7,14 @@ const generateProgressCallback = (_step: number, message: string) => {
     chrome.runtime.sendMessage({ answer: message });
 };
 
-const cm = new ChatRestModule();
+const cm = new ChatModule();
+
+cm.reload("vicuna-v1-7b-q4f32_0")
 
 // listen for a request message from the content script
-chrome.runtime.onMessage.addListener(async function (request) {
-    // check if the request contains a message that the user sent a new message
-    if (request.input) {
-        const response = await cm.generate(request.input, generateProgressCallback);
-    }
-});
+// chrome.runtime.onMessage.addListener(async function (request) {
+//     // check if the request contains a message that the user sent a new message
+//     if (request.input) {
+//         const response = await cm.generate(request.input, generateProgressCallback);
+//     }
+// });
