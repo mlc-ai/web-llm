5cf7ef2c8f019c0159daf5889c0d1e48dd7a26ed
Charlie Ruan <53290280+CharlieFRuan@users.noreply.github.com>
2024-05-25 19:43:35 -0400

    [Fix] Rename TVM_HOME TVM_SOURCE_DIR

--- scripts/prep_deps.sh
+++ scripts/prep_deps.sh
@@ -5,19 +5,19 @@ set -euxo pipefail
 emcc --version
 npm --version
 
-TVM_HOME_SET="${TVM_HOME:-}"
+TVM_SOURCE_DIR_SET="${TVM_SOURCE_DIR:-}"
 
-if [[ -z ${TVM_HOME_SET} ]]; then
+if [[ -z ${TVM_SOURCE_DIR_SET} ]]; then
     if [[ ! -d "3rdparty/tvm-unity" ]]; then
-        echo "Do not find TVM_HOME env variable, cloning a version as source".
+        echo "Do not find TVM_SOURCE_DIR env variable, cloning a version as source".
         git clone https://github.com/mlc-ai/relax 3rdparty/tvm-unity --recursive
     fi
-    export TVM_HOME="${TVM_HOME:-3rdparty/tvm-unity}"
+    export TVM_SOURCE_DIR="${TVM_SOURCE_DIR:-3rdparty/tvm-unity}"
 fi
 
-cd ${TVM_HOME}/web && make && npm install && npm run build && cd -
+cd ${TVM_SOURCE_DIR}/web && make && npm install && npm run build && cd -
 rm -rf tvm_home
-ln -s ${TVM_HOME} tvm_home
+ln -s ${TVM_SOURCE_DIR} tvm_home
 npm install
 cd examples/simple-chat
 npm install
