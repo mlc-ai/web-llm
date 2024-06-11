f496f0e8968495e37260e95645e8fa3be1c61ce5
John Robinson <jrobinson@jackhenry.com>
2024-06-11 08:53:37 -0400

    feat(examples): complete function calling example

--- examples/function-calling/src/function_calling.ts
+++ examples/function-calling/src/function_calling.ts
@@ -18,17 +18,41 @@ async function main() {
     { initProgressCallback: initProgressCallback },
   );
 
+  const availableFunctions = {
+    // mock get weather function
+    getCurrentWeather: function (location, unit) {
+      return {
+        location,
+        unit,
+        weather: {
+          wind: "10 mph",
+          direction: "s",
+          temperature: "50",
+        },
+      };
+    },
+  };
+
+  // store messages history
+  const messages: webllm.ChatCompletionMessage[] = [
+    {
+      role: "user",
+      content:
+        "What is the current weather in celsius in Pittsburgh and Tokyo?",
+    },
+  ];
+
   const tools: Array<webllm.ChatCompletionTool> = [
     {
       type: "function",
       function: {
-        name: "get_current_weather",
+        name: "getCurrentWeather",
         description: "Get the current weather in a given location",
         parameters: {
           type: "object",
           properties: {
             location: {
-              type: "string",
+              type: "location",
               description: "The city and state, e.g. San Francisco, CA",
             },
             unit: { type: "string", enum: ["celsius", "fahrenheit"] },
@@ -40,41 +64,61 @@ async function main() {
   ];
 
   const request: webllm.ChatCompletionRequest = {
-    stream: true, // works with stream as well, where the last chunk returns tool_calls
-    stream_options: { include_usage: true },
-    messages: [
-      {
-        role: "user",
-        content:
-          "What is the current weather in celsius in Pittsburgh and Tokyo?",
-      },
-    ],
+    stream: false, // works with stream as well, where the last chunk returns tool_calls
+    // stream_options: { include_usage: true },
+    messages,
     tool_choice: "auto",
     tools: tools,
   };
 
-  if (!request.stream) {
-    const reply0 = await engine.chat.completions.create(request);
-    console.log(reply0.choices[0]);
-    console.log(reply0.usage);
-  } else {
-    // If streaming, the last chunk returns tool calls
-    const asyncChunkGenerator = await engine.chat.completions.create(request);
-    let message = "";
-    let lastChunk: webllm.ChatCompletionChunk | undefined;
-    let usageChunk: webllm.ChatCompletionChunk | undefined;
-    for await (const chunk of asyncChunkGenerator) {
-      console.log(chunk);
-      message += chunk.choices[0]?.delta?.content || "";
-      setLabel("generate-label", message);
-      if (!chunk.usage) {
-        lastChunk = chunk;
-      }
-      usageChunk = chunk;
-    }
-    console.log(lastChunk!.choices[0].delta);
-    console.log(usageChunk!.usage);
+  const reply0 = await engine.chat.completions.create(request);
+  console.log(reply0.choices[0].message);
+  // Errors here - system doesn't like that content is null, skipping for now...
+  // messages.push(reply0.choices[0].message);
+  console.log(reply0.usage);
+  console.log("tool_calls:", reply0.choices[0].message.tool_calls);
+
+  const toolCalls = reply0.choices[0].message.tool_calls;
+
+  if (toolCalls) {
+    toolCalls.forEach((tc) => {
+      const args = JSON.parse(tc.function.arguments);
+      const result = availableFunctions[tc.function.name].apply(
+        this,
+        Object.values(args),
+      );
+      messages.push({
+        tool_call_id: tc.id,
+        role: "tool",
+        name: tc.function.name,
+        content: JSON.stringify(result),
+      });
+    });
+    const reply1 = await engine.chat.completions.create({
+      messages,
+    });
+
+    console.log("second reply", reply1);
   }
+  // const request2: webllm.ChatCompletionRequest = {
+  //   stream: false, // works with stream as well, where the last chunk returns tool_calls
+  //   // stream_options: { include_usage: true },
+  //   messages: [
+  //     ...messages,
+  //     {
+  //       role: "user",
+  //       content: "What is the current weather in farenheit in Denver?",
+  //     },
+  //   ],
+  //   tool_choice: "auto",
+  //   tools: tools,
+  // };
+
+  // const reply1 = await engine.chat.completions.create(request2);
+  // console.log(reply1.choices[0]);
+  // console.log(reply1.usage);
+  // console.log('')
+  // // messages.push(reply1.choices[0].message);
 }
 
 main();
