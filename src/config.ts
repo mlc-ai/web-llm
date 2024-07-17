f3390fdfe01ede857768dff848b1c63e2fed43b6
Charlie Ruan <53290280+CharlieFRuan@users.noreply.github.com>
2024-07-16 20:24:21 -0400

    [Model] Add SmoLM models (#508)
    Add `q0f16`, `q0f32`, `q4f16_1`, `q4f32_1` for each of the three sizes.

--- src/config.ts
+++ src/config.ts
@@ -544,6 +544,170 @@ export const prebuiltAppConfig: AppConfig = {
         sliding_window_size: -1,
       },
     },
+    // SmolLM
+    {
+      model: "https://huggingface.co/mlc-ai/SmolLM-1.7B-Instruct-q0f16-MLC",
+      model_id: "SmolLM-1.7B-Instruct-q0f16-MLC",
+      model_lib:
+        modelLibURLPrefix +
+        modelVersion +
+        "/SmolLM-1.7B-Instruct-q0f16-ctx2k_cs1k-webgpu.wasm",
+      vram_required_MB: 3736.19,
+      low_resource_required: true,
+      required_features: ["shader-f16"],
+      overrides: {
+        context_window_size: 2048,
+      },
+    },
+    {
+      model: "https://huggingface.co/mlc-ai/SmolLM-1.7B-Instruct-q0f32-MLC",
+      model_id: "SmolLM-1.7B-Instruct-q0f32-MLC",
+      model_lib:
+        modelLibURLPrefix +
+        modelVersion +
+        "/SmolLM-1.7B-Instruct-q0f32-ctx2k_cs1k-webgpu.wasm",
+      vram_required_MB: 7432.38,
+      low_resource_required: false,
+      overrides: {
+        context_window_size: 2048,
+      },
+    },
+    {
+      model: "https://huggingface.co/mlc-ai/SmolLM-1.7B-Instruct-q4f16_1-MLC",
+      model_id: "SmolLM-1.7B-Instruct-q4f16_1-MLC",
+      model_lib:
+        modelLibURLPrefix +
+        modelVersion +
+        "/SmolLM-1.7B-Instruct-q4f16_1-ctx2k_cs1k-webgpu.wasm",
+      vram_required_MB: 1390.19,
+      low_resource_required: true,
+      required_features: ["shader-f16"],
+      overrides: {
+        context_window_size: 2048,
+      },
+    },
+    {
+      model: "https://huggingface.co/mlc-ai/SmolLM-1.7B-Instruct-q4f32_1-MLC",
+      model_id: "SmolLM-1.7B-Instruct-q4f32_1-MLC",
+      model_lib:
+        modelLibURLPrefix +
+        modelVersion +
+        "/SmolLM-1.7B-Instruct-q4f32_1-ctx2k_cs1k-webgpu.wasm",
+      vram_required_MB: 1924.38,
+      low_resource_required: true,
+      overrides: {
+        context_window_size: 2048,
+      },
+    },
+
+    {
+      model: "https://huggingface.co/mlc-ai/SmolLM-360M-Instruct-q0f16-MLC",
+      model_id: "SmolLM-360M-Instruct-q0f16-MLC",
+      model_lib:
+        modelLibURLPrefix +
+        modelVersion +
+        "/SmolLM-360M-Instruct-q0f16-ctx2k_cs1k-webgpu.wasm",
+      vram_required_MB: 791.99,
+      low_resource_required: true,
+      required_features: ["shader-f16"],
+      overrides: {
+        context_window_size: 2048,
+      },
+    },
+    {
+      model: "https://huggingface.co/mlc-ai/SmolLM-360M-Instruct-q0f32-MLC",
+      model_id: "SmolLM-360M-Instruct-q0f32-MLC",
+      model_lib:
+        modelLibURLPrefix +
+        modelVersion +
+        "/SmolLM-360M-Instruct-q0f32-ctx2k_cs1k-webgpu.wasm",
+      vram_required_MB: 1583.99,
+      low_resource_required: true,
+      overrides: {
+        context_window_size: 2048,
+      },
+    },
+    {
+      model: "https://huggingface.co/mlc-ai/SmolLM-360M-Instruct-q4f16_1-MLC",
+      model_id: "SmolLM-360M-Instruct-q4f16_1-MLC",
+      model_lib:
+        modelLibURLPrefix +
+        modelVersion +
+        "/SmolLM-360M-Instruct-q4f16_1-ctx2k_cs1k-webgpu.wasm",
+      vram_required_MB: 296.06,
+      low_resource_required: true,
+      required_features: ["shader-f16"],
+      overrides: {
+        context_window_size: 2048,
+      },
+    },
+    {
+      model: "https://huggingface.co/mlc-ai/SmolLM-360M-Instruct-q4f32_1-MLC",
+      model_id: "SmolLM-360M-Instruct-q4f32_1-MLC",
+      model_lib:
+        modelLibURLPrefix +
+        modelVersion +
+        "/SmolLM-360M-Instruct-q4f32_1-ctx2k_cs1k-webgpu.wasm",
+      vram_required_MB: 419.61,
+      low_resource_required: true,
+      overrides: {
+        context_window_size: 2048,
+      },
+    },
+    {
+      model: "https://huggingface.co/mlc-ai/SmolLM-135M-Instruct-q0f16-MLC",
+      model_id: "SmolLM-135M-Instruct-q0f16-MLC",
+      model_lib:
+        modelLibURLPrefix +
+        modelVersion +
+        "/SmolLM-135M-Instruct-q0f16-ctx2k_cs1k-webgpu.wasm",
+      vram_required_MB: 314.69,
+      low_resource_required: true,
+      required_features: ["shader-f16"],
+      overrides: {
+        context_window_size: 2048,
+      },
+    },
+    {
+      model: "https://huggingface.co/mlc-ai/SmolLM-135M-Instruct-q0f32-MLC",
+      model_id: "SmolLM-135M-Instruct-q0f32-MLC",
+      model_lib:
+        modelLibURLPrefix +
+        modelVersion +
+        "/SmolLM-135M-Instruct-q0f32-ctx2k_cs1k-webgpu.wasm",
+      vram_required_MB: 629.38,
+      low_resource_required: true,
+      overrides: {
+        context_window_size: 2048,
+      },
+    },
+    {
+      model: "https://huggingface.co/mlc-ai/SmolLM-135M-Instruct-q4f16_1-MLC",
+      model_id: "SmolLM-135M-Instruct-q4f16_1-MLC",
+      model_lib:
+        modelLibURLPrefix +
+        modelVersion +
+        "/SmolLM-135M-Instruct-q4f16_1-ctx2k_cs1k-webgpu.wasm",
+      vram_required_MB: 130.33,
+      low_resource_required: true,
+      required_features: ["shader-f16"],
+      overrides: {
+        context_window_size: 2048,
+      },
+    },
+    {
+      model: "https://huggingface.co/mlc-ai/SmolLM-135M-Instruct-q4f32_1-MLC",
+      model_id: "SmolLM-135M-Instruct-q4f32_1-MLC",
+      model_lib:
+        modelLibURLPrefix +
+        modelVersion +
+        "/SmolLM-135M-Instruct-q4f32_1-ctx2k_cs1k-webgpu.wasm",
+      vram_required_MB: 196.54,
+      low_resource_required: true,
+      overrides: {
+        context_window_size: 2048,
+      },
+    },
     // Gemma-2B
     {
       model: "https://huggingface.co/mlc-ai/gemma-2b-it-q4f16_1-MLC",
