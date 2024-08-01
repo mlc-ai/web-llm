a3f8f4af105f35c06a6c4e5fd62cd2cfcac00373
Charlie Ruan <53290280+CharlieFRuan@users.noreply.github.com>
2024-07-31 20:04:17 -0400

    [Model] Add gemma-2 2b and 9b

--- examples/simple-chat-ts/src/simple_chat.ts
+++ examples/simple-chat-ts/src/simple_chat.ts
@@ -342,7 +342,7 @@ let engine: webllm.MLCEngineInterface;
 if (useWebWorker) {
   engine = new webllm.WebWorkerMLCEngine(
     new Worker(new URL("./worker.ts", import.meta.url), { type: "module" }),
-    { appConfig },
+    { appConfig, logLevel: "INFO" },
   );
 } else {
   engine = new webllm.MLCEngine({ appConfig });
