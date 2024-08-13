f707d8de8da0e04b29a926e36e253fa9936dcedc
Charlie Ruan <53290280+CharlieFRuan@users.noreply.github.com>
2024-08-13 14:59:39 -0400

    Update multi-models example to have parallel inference

--- examples/multi-models/src/multi_models.ts
+++ examples/multi-models/src/multi_models.ts
@@ -1,3 +1,11 @@
+/**
+ * This example demonstrates loading multiple models in the same engine concurrently.
+ * sequentialGeneration() shows inference each model one at a time.
+ * parallelGeneration() shows inference both models at the same time.
+ * This example uses MLCEngine, but the same idea applies to WebWorkerMLCEngine and
+ * ServiceWorkerMLCEngine as well.
+ */
+
 import * as webllm from "@mlc-ai/web-llm";
 
 function setLabel(id: string, text: string) {
@@ -8,60 +16,60 @@ function setLabel(id: string, text: string) {
   label.innerText = text;
 }
 
-/**
- * Chat completion (OpenAI style) with streaming, with two models in the pipeline.
- */
-async function mainStreaming() {
-  const initProgressCallback = (report: webllm.InitProgressReport) => {
-    setLabel("init-label", report.text);
-  };
-  const selectedModel1 = "Phi-3-mini-4k-instruct-q4f32_1-MLC-1k";
-  const selectedModel2 = "gemma-2-2b-it-q4f32_1-MLC-1k";
+const initProgressCallback = (report: webllm.InitProgressReport) => {
+  setLabel("init-label", report.text);
+};
 
+// Prepare request for each model, same for both methods
+const selectedModel1 = "Phi-3-mini-4k-instruct-q4f32_1-MLC-1k";
+const selectedModel2 = "gemma-2-2b-it-q4f32_1-MLC-1k";
+const prompt1 = "Tell me about California in 3 short sentences.";
+const prompt2 = "Tell me about New York City in 3 short sentences.";
+setLabel("prompt-label-1", `(with model ${selectedModel1})\n` + prompt1);
+setLabel("prompt-label-2", `(with model ${selectedModel2})\n` + prompt2);
+
+const request1: webllm.ChatCompletionRequestStreaming = {
+  stream: true,
+  stream_options: { include_usage: true },
+  messages: [{ role: "user", content: prompt1 }],
+  model: selectedModel1, // without specifying it, error will throw due to ambiguity
+  max_tokens: 128,
+};
+
+const request2: webllm.ChatCompletionRequestStreaming = {
+  stream: true,
+  stream_options: { include_usage: true },
+  messages: [{ role: "user", content: prompt2 }],
+  model: selectedModel2, // without specifying it, error will throw due to ambiguity
+  max_tokens: 256,
+};
+
+/**
+ * Streaming chat completion, with two models in the engine running one at a time.
+ */
+async function sequentialGeneration() {
   const engine: webllm.MLCEngineInterface = await webllm.CreateMLCEngine(
     [selectedModel1, selectedModel2],
     { initProgressCallback: initProgressCallback },
   );
 
-  const request1: webllm.ChatCompletionRequest = {
-    stream: true,
-    stream_options: { include_usage: true },
-    messages: [
-      { role: "user", content: "Provide me three US states." },
-      { role: "assistant", content: "California, New York, Pennsylvania." },
-      { role: "user", content: "Two more please!" },
-    ],
-    model: selectedModel1, // without specifying it, error will throw due to ambiguity
-  };
-
-  const request2: webllm.ChatCompletionRequest = {
-    stream: true,
-    stream_options: { include_usage: true },
-    messages: [
-      { role: "user", content: "Provide me three cities in NY." },
-      { role: "assistant", content: "New York, Binghamton, Buffalo." },
-      { role: "user", content: "Two more please!" },
-    ],
-    model: selectedModel2, // without specifying it, error will throw due to ambiguity
-  };
-
   const asyncChunkGenerator1 = await engine.chat.completions.create(request1);
-  let message = "";
+  let message1 = "";
   for await (const chunk of asyncChunkGenerator1) {
-    console.log(chunk);
-    message += chunk.choices[0]?.delta?.content || "";
-    setLabel("generate-label", message);
+    // console.log(chunk);
+    message1 += chunk.choices[0]?.delta?.content || "";
+    setLabel("generate-label-1", message1);
     if (chunk.usage) {
       console.log(chunk.usage); // only last chunk has usage
     }
     // engine.interruptGenerate();  // works with interrupt as well
   }
   const asyncChunkGenerator2 = await engine.chat.completions.create(request2);
-  message += "\n\n";
+  let message2 = "";
   for await (const chunk of asyncChunkGenerator2) {
-    console.log(chunk);
-    message += chunk.choices[0]?.delta?.content || "";
-    setLabel("generate-label", message);
+    // console.log(chunk);
+    message2 += chunk.choices[0]?.delta?.content || "";
+    setLabel("generate-label-2", message2);
     if (chunk.usage) {
       console.log(chunk.usage); // only last chunk has usage
     }
@@ -73,4 +81,52 @@ async function mainStreaming() {
   console.log("Final message 2:\n", await engine.getMessage(selectedModel2));
 }
 
-mainStreaming();
+/**
+ * Streaming chat completion, with two models in the engine running concurrently.
+ */
+async function parallelGeneration() {
+  const engine: webllm.MLCEngineInterface = await webllm.CreateMLCEngine(
+    [selectedModel1, selectedModel2],
+    { initProgressCallback: initProgressCallback },
+  );
+
+  // We can serve the two requests concurrently
+  let message1 = "";
+  let message2 = "";
+
+  async function getModel1Response() {
+    const asyncChunkGenerator1 = await engine.chat.completions.create(request1);
+    for await (const chunk of asyncChunkGenerator1) {
+      // console.log(chunk);
+      message1 += chunk.choices[0]?.delta?.content || "";
+      setLabel("generate-label-1", message1);
+      if (chunk.usage) {
+        console.log(chunk.usage); // only last chunk has usage
+      }
+      // engine.interruptGenerate();  // works with interrupt as well
+    }
+  }
+
+  async function getModel2Response() {
+    const asyncChunkGenerator2 = await engine.chat.completions.create(request2);
+    for await (const chunk of asyncChunkGenerator2) {
+      // console.log(chunk);
+      message2 += chunk.choices[0]?.delta?.content || "";
+      setLabel("generate-label-2", message2);
+      if (chunk.usage) {
+        console.log(chunk.usage); // only last chunk has usage
+      }
+      // engine.interruptGenerate();  // works with interrupt as well
+    }
+  }
+
+  await Promise.all([getModel1Response(), getModel2Response()]);
+
+  // without specifying from which model to get message, error will throw due to ambiguity
+  console.log("Final message 1:\n", await engine.getMessage(selectedModel1));
+  console.log("Final message 2:\n", await engine.getMessage(selectedModel2));
+}
+
+// Pick one to run
+// sequentialGeneration();
+parallelGeneration();
