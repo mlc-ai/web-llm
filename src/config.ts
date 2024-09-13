b55658176b2d2224e9bf03ec241e8c4abaec23d8
AMKCode <aomankit2002@gmail.com>
2024-09-13 14:31:55 -0400

    added Hermes 3 to function calling models

--- src/config.ts
+++ src/config.ts
@@ -294,6 +294,8 @@ export const functionCallingModelIds = [
   "Hermes-2-Pro-Llama-3-8B-q4f16_1-MLC",
   "Hermes-2-Pro-Llama-3-8B-q4f32_1-MLC",
   "Hermes-2-Pro-Mistral-7B-q4f16_1-MLC",
+  "Hermes-3-Llama-3.1-8B-q4f32_1-MLC",
+  "Hermes-3-Llama-3.1-8B-q4f16_1-MLC",
 ];
 
 /**
