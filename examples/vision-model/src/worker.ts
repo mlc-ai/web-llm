0d6269ceebe586715e11224fd5ee703a319c20d3
Charlie Ruan <53290280+CharlieFRuan@users.noreply.github.com>
2024-09-25 22:17:19 -0700

    [Version] Bump version to 0.2.68 (#577)
    ### Change
    
    - https://github.com/mlc-ai/web-llm/pull/574
      - Enable image input for web/service worker
    
    ### TVMjs
    - No change, version `0.18.0-dev0` just like 0.2.66
    - Built at
    https://github.com/apache/tvm/commit/30fb16a5e1d564ffa8533cf154c0ba2ea06dfd43

---
+++ examples/vision-model/src/worker.ts
@@ -0,0 +1,7 @@
+import { WebWorkerMLCEngineHandler } from "@mlc-ai/web-llm";
+
+const handler = new WebWorkerMLCEngineHandler();
+
+self.onmessage = (msg: MessageEvent) => {
+  handler.onmessage(msg);
+};
