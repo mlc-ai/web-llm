c2979551ee7b328d83e699a797d93bdf8d90ce05
Akaash Parthasarathy <akaashrp@gmail.com>
2025-11-02 01:47:54 -0400

    Bump modelVersion to v0_2_80

--- src/config.ts
+++ src/config.ts
@@ -288,7 +288,7 @@ export interface AppConfig {
  * @note The model version does not have to match the npm version, since not each npm update
  * requires an update of the model libraries.
  */
-export const modelVersion = "v0_2_48";
+export const modelVersion = "v0_2_80";
 export const modelLibURLPrefix =
   "https://raw.githubusercontent.com/mlc-ai/binary-mlc-llm-libs/main/web-llm-models/";
 
