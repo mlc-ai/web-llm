926ab86e275adffa95ef48efe722905920ef2022
Akaash Parthasarathy <43900735+akaashrp@users.noreply.github.com>
2026-03-13 00:05:44 -0400

    [Version] Bump version to 0.2.82 (#785)

--- docs/conf.py
+++ docs/conf.py
@@ -17,8 +17,8 @@ copyright = "2023, %s" % author
 
 # Version information.
 
-version = "0.2.81"
-release = "0.2.81"
+version = "0.2.82"
+release = "0.2.82"
 
 extensions = [
     "sphinx_tabs.tabs",
