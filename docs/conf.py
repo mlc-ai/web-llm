63d8588bfee688ebdb49ea1d7ca345fa5d157a7a
Akaash Parthasarathy <akaashrp@gmail.com>
2026-09-07 13:47:31 -0400

    [Release] Prepare WebLLM 0.2.85

--- docs/conf.py
+++ docs/conf.py
@@ -17,8 +17,8 @@ copyright = "2023, %s" % author
 
 # Version information.
 
-version = "0.2.84"
-release = "0.2.84"
+version = "0.2.85"
+release = "0.2.85"
 
 extensions = [
     "sphinx_tabs.tabs",
